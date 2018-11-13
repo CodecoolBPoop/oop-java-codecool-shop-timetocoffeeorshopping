318f975989fee959a85f33abdb36b4dd8543e49d
readdeo <readdeo91@gmail.com>
2018-11-13 14:49:39 +0100

    Header, footer and sidebar are now properly separated, sidebar menu active highlight working now.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -35,6 +35,7 @@ public class ProductController extends HttpServlet {
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        context.setVariable("page", "index");
         engine.process("product/indexv2.html", context, resp.getWriter());
     }
 
