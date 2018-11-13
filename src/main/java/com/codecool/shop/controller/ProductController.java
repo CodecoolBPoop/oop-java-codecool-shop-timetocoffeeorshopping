a6071088a66162393c71ea03346229ffbb837360
cortinadampezzo <kisgans@gmail.com>
2018-11-13 14:16:08 +0100

    sort is suppliers implemented

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -24,18 +26,24 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
         context.setVariable("categories", productCategoryDataStore.getAll());
+        context.setVariable("suppliers", supplierDataStore.getAll());
 
         if (req.getParameter("category") != null) {
             context.setVariable("products", productDataStore.getBy
                     (productCategoryDataStore.find(Integer.valueOf(req.getParameter("category")))));
-            System.out.println(productCategoryDataStore.find(Integer.valueOf(req.getParameter("category"))));
             context.setVariable("category", productCategoryDataStore.find
                     (Integer.parseInt(req.getParameter("category"))).getName());
+        } else if (req.getParameter("supplier") != null) {
+            context.setVariable("products", productDataStore.getBy
+                    (supplierDataStore.find(Integer.valueOf(req.getParameter("supplier")))));
+            context.setVariable("supplier", supplierDataStore.find
+                    (Integer.parseInt(req.getParameter("supplier"))).getName());
         } else {
             context.setVariable("products", productDataStore.getAll());
         }
