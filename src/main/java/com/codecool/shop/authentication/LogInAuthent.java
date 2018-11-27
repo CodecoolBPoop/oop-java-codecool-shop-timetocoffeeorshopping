4e0263e54ebec05a82b5cf4104fde09af0c77452
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-11-27 13:17:44 +0100

    create authentication class but method is not working

---
+++ src/main/java/com/codecool/shop/authentication/LogInAuthent.java
@@ -0,0 +1,7 @@
+package com.codecool.shop.authentication;
+
+public class LogInAuthent {
+    static public boolean verifyPassword(String name, String password) {
+        return true;
+    }
+}
--- src/main/java/com/codecool/shop/controller/LoginPage.java
+++ src/main/java/com/codecool/shop/controller/LoginPage.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.authentication.LogInAuthent;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -8,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/login"})
@@ -19,5 +21,24 @@ public class LoginPage extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
         engine.process("login.html", context, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        boolean isVerified = LogInAuthent.verifyPassword(name, password);
+        context.setVariable("isVerified", isVerified);
+
+        if (isVerified) {
+            HttpSession session = request.getSession();
+            session.setAttribute("name",name);
+            engine.process("login.html", context, response.getWriter());
+
+        } else {
+            engine.process("login.html", context, response.getWriter());
+        }
     }
 }
