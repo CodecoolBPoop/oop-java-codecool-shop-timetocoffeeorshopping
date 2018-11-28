4e191faecc7ad46db7f7c8e50088b0f1a96798b7
Szajler Indira <szajler.indira@gmail.com>
2018-11-28 13:27:47 +0100

    password hashing is refactored

--- src/main/java/com/codecool/shop/password/HashAndAuthenticate.java
+++ src/main/java/com/codecool/shop/password/HashAndAuthenticate.java
@@ -1,5 +1,8 @@
 package com.codecool.shop.password;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.MessageDigest;
@@ -15,37 +18,45 @@ public class HashAndAuthenticate {
     https://howtodoinjava.com/security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
      */
     public static String getSecurePassword(String painpassword) throws NoSuchAlgorithmException {
-        byte[] salt = getSalt();
-        String securePassword = get_SHA_256_SecurePassword(painpassword, salt);
+
+        String securePassword = get_SHA_256_SecurePassword(painpassword);
         return securePassword;
     }
 
-    private static String get_SHA_256_SecurePassword(String plainPassword, byte[] salt) {
-        String generatedPassword = null;
+    private static String get_SHA_256_SecurePassword(String plainPassword) {
 
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(salt);
-            byte[] bytes = md.digest(plainPassword.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        return generatedPassword;
+        String sha256hex = Hashing.sha256()
+                .hashString(plainPassword, StandardCharsets.UTF_8)
+                .toString();
+        return sha256hex;
+
+
+        //String sha256hex = org.apache.commons.codec.digest.DigestUtils.sha256Hex(plainPassword);
+//        String generatedPassword = null;
+//
+//        try {
+//            MessageDigest md = MessageDigest.getInstance("SHA-256");
+//            md.update(salt);
+//            byte[] bytes = md.digest(plainPassword.getBytes());
+//            StringBuilder sb = new StringBuilder();
+//            for(int i=0; i< bytes.length ;i++)
+//            {
+//                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+//            }
+//            generatedPassword = sb.toString();
+//        }
+//        catch (NoSuchAlgorithmException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        return generatedPassword;
     }
 
     //Add salt
-    private static byte[] getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
-    }
+//    private static byte[] getSalt() throws NoSuchAlgorithmException {
+//        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+//        byte[] salt = new byte[16];
+//        sr.nextBytes(salt);
+//        return salt;
+//    }
 }
