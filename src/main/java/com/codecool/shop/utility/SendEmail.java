2c7ce6bd5d740fdffc5958f39c07a71d616e3ad4
Szajler Indira <szajler.indira@gmail.com>
2018-11-29 11:09:09 +0100

    sending email feature started

---
+++ src/main/java/com/codecool/shop/utility/SendEmail.java
@@ -0,0 +1,69 @@
+package com.codecool.shop.utility;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+
+public class SendEmail {
+
+    public static void sendEmail(String email) throws MessagingException {
+//        Properties prop = new Properties();
+//        prop.put("mail.smtp.auth", true);
+//        prop.put("mail.smtp.starttls.enable", "true");
+//        prop.put("mail.smtp.host", "smtp.mailtrap.io");
+//        prop.put("mail.smtp.port", "25");
+//        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+//
+//
+//        Session session = Session.getDefaultInstance(prop, new Authenticator() {
+//            @Override
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication(username, password);
+//            }
+//        });
+//
+//
+
+
+        // Recipient's email ID needs to be mentioned.
+        String to = "szajler.indira@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "web@gmail.com";
+
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+
+    }
+}
